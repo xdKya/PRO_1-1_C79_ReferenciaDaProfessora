e1a117d240a8fd0bc75c1d93df3e10288e00fbcf
xdKya <90745676+xdKya@users.noreply.github.com>
2023-11-08 11:11:03 -0300

    Update Meteors.js

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -28,43 +28,6 @@ export default class MeteorScreen extends Component {
 
     
 
-    renderItem = ({ item }) => {
-        let meteor = item
-        let bg_img, speed, size;
-        if (meteor.threat_score <= 30) {
-            bg_img = require("../assets/meteor_bg1.png")
-            speed = require("../assets/meteor_speed3.gif")
-            size = 100
-        } else if (meteor.threat_score <= 75) {
-            bg_img = require("../assets/meteor_bg2.png")
-            speed = require("../assets/meteor_speed3.gif")
-            size = 150
-        } else {
-            bg_img = require("../assets/meteor_bg3.png")
-            speed = require("../assets/meteor_speed3.gif")
-            size = 200
-        }
-        return (
-            <View>
-                <ImageBackground source={bg_img} style={styles.backgroundImage}>
-                    <View styles={styles.gifContainer}>
-                        <Image source={speed} style={{ width: size, height: size, alignSelf: "center" }}></Image>
-                        <View>
-                            <Text style={[styles.cardTitle, { marginTop: 400, marginLeft: 50 }]}>{item.name}</Text>
-                            <Text style={[styles.cardText, { marginTop: 20, marginLeft: 50 }]}>Mais próximo da Terra - {item.close_approach_data[0].close_approach_date_full}</Text>
-                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diâmetro Mínimo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_min}</Text>
-                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diâmetro Máximo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_max}</Text>
-                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Velocidade (KM/H) - {item.close_approach_data[0].relative_velocity.kilometers_per_hour}</Text>
-                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Distância da Terra (KM) - {item.close_approach_data[0].miss_distance.kilometers}</Text>
-                        </View>
-                    </View>
-                </ImageBackground>
-            </View>
-        );
-    };
-
-    keyExtractor = (item, index) => index.toString();
-
     render() {
         if (Object.keys(this.state.meteors).length === 0) {
             return (
@@ -78,28 +41,11 @@ export default class MeteorScreen extends Component {
                 </View>
             )
         } else {
-            let meteor_arr = Object.keys(this.state.meteors).map(meteor_date => {
-                return this.state.meteors[meteor_date]
-            })
-            let meteors = [].concat.apply([], meteor_arr);
-            meteors.forEach(function (element) {
-                let diameter = (element.estimated_diameter.kilometers.estimated_diameter_min + element.estimated_diameter.kilometers.estimated_diameter_max) / 2
-                let threatScore = (diameter / element.close_approach_data[0].miss_distance.kilometers) * 1000000000
-                element.threat_score = threatScore;
-            });
-            meteors.sort(function (a, b) {
-                return b.threat_score - a.threat_score
-            })
-            meteors = meteors.slice(0, 5)
             return (
                 <View style={styles.container}>
                     <SafeAreaView style={styles.droidSafeArea} />
-                    <FlatList
-                        keyExtractor={this.keyExtractor}
-                        data={meteors}
-                        renderItem={this.renderItem}
-                        horizontal={true}
-                    />
+                Tela de Meteoros!
+                   
                 </View >
             );
         }
